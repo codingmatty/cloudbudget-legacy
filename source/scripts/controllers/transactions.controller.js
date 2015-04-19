d479230572201dd49e9f65121a5b0a25ece66cca
Matthew Jacobs <m.a.jacobs@outlook.com>
2015-04-18 22:37:44 -0500

    Fixed #17

--- source/scripts/controllers/transactions.controller.js
+++ source/scripts/controllers/transactions.controller.js
@@ -10,6 +10,7 @@ angular.module('CloudBudget')
      var views = ['list', 'form', 'show'];
      var currentView = 'list';
      var transactionsRest;
+     var originalTransaction = null;
      // endregion
 
      // region Public Variables
@@ -50,8 +51,10 @@ angular.module('CloudBudget')
 
      var setCurrentTransaction = function(transaction) {
        vm.currentTransaction = transaction;
-       if (transaction && transaction.date) {
-         vm.currentTransaction.date = new Date(transaction.date);
+       if (transaction) {
+         if (transaction.date) {
+           vm.currentTransaction.date = new Date(transaction.date);
+         }
        }
      }
      // endregion
@@ -64,6 +67,7 @@ angular.module('CloudBudget')
      vm.listTransactions = function() {
        currentView = 'list';
        vm.showFormRow = false;
+       originalTransaction = null;
      };
 
      vm.showTransaction = function(transaction) {
@@ -73,6 +77,7 @@ angular.module('CloudBudget')
 
      vm.createTransaction = function() {
        setCurrentTransaction({cleared: false});
+       originalTransaction = null;
        if (!matchmedia.isDesktop()) {
          currentView = 'form';
        } else {
@@ -83,12 +88,35 @@ angular.module('CloudBudget')
 
      vm.editTransaction = function(transaction) {
        setCurrentTransaction(transaction);
+
        if (!matchmedia.isDesktop()) {
          currentView = 'form';
        } else {
+         {
+           originalTransaction = {};
+           originalTransaction._id = transaction._id;
+           if (transaction.date) {
+             originalTransaction.date = new Date(transaction.date);
+           }
+           originalTransaction.payee = transaction.payee;
+           originalTransaction.amount = transaction.amount;
+           originalTransaction.account = transaction.account;
+           originalTransaction.cleared = transaction.cleared;
+           originalTransaction.tag = transaction.tag;
+         }
          currentView = 'list';
          vm.showFormRow = true;
+         var transactionIdx = vm.transactions.indexOf(transaction);
+         vm.transactions.splice(transactionIdx, 1);
        }
+     };
+
+     vm.cancelForm = function() {
+       if (originalTransaction) {
+         vm.transactions.push(originalTransaction);
+       }
+       setCurrentTransaction({});
+       vm.listTransactions();
      };
 
      vm.deleteTransaction = function(transaction) {
@@ -99,12 +127,14 @@ angular.module('CloudBudget')
            SpendingService.unregisterTransaction(transaction);
          });
        }
-     }
+     };
 
      vm.saveTransaction = function(transaction) {
        if (transaction._id) {
-         transactionsRest.customPUT(transaction, transaction._id);
-         SpendingService.registerTransaction(transaction);
+         transactionsRest.customPUT(transaction, transaction._id).then(function(one) {
+           vm.transactions.push(transaction);
+           SpendingService.registerTransaction(transaction);
+         });
        } else {
          transactionsRest.post(transaction).then(function(postedTransaction) {
            transaction._id = postedTransaction._id;
@@ -114,7 +144,11 @@ angular.module('CloudBudget')
        }
        setCurrentTransaction({});
        vm.listTransactions();
-     }
+     };
+     
+     vm.orderTransactions = function(transaction) {
+       return -(new Date(transaction.date).getTime());
+     };
      // endregion
      // endregion
 
