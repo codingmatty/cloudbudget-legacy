ad487499b1181d5bab24b799aa2ad0c44ffd12f7
Matthew Jacobs <m.a.jacobs@outlook.com>
2015-04-21 20:05:00 -0500

    Refactored server side route files. Closes #21

--- server/routes/plan.js
+++ server/routes/plan.js
@@ -3,76 +3,48 @@ var Plan       = require('../models/plan');
 
 var router = express.Router();
 
-// Routes that end in /plan
-// ----------------------------------------------------
-router.route('')
-/* GET /api/plan
- * params:
- */
-  .get(function(req, res) {
+var getPlan = function(req, res) {
   var query = Plan.findOne();
 
   // Now send back resulting plan
   query.exec(function(err, plan) {
-    if (err)
-      res.send(err);
-
+    if (err) res.send(err);
     res.json(plan);
   });
-})
+};
+
+var updatePlan = function(req, res) {
+  Plan.findOne({}, function(err, plan) {
+    if (err) res.send(err);
+
+    if (plan) {
+      // Only save fields that have been passed, without editing ones that have not.
+      if (req.body.monthlyIncome !== undefined) { plan.monthlyIncome = req.body.monthlyIncome; }
+      if (req.body.monthlyBills !== undefined) { plan.monthlyBills = req.body.monthlyBills; }
+      if (req.body.monthlySavings !== undefined) { plan.monthlySavings = req.body.monthlySavings; }
+    } else {
+      plan = new Plan({
+        monthlyIncome: req.body.monthlyIncome,
+        monthlyBills: req.body.monthlyBills,
+        monthlySavings: req.body.monthlySavings
+      });
+    }
+
+    plan.save(function(err) {
+      if (err) res.send(err);
+      res.json({ message: 'Plan Updated!' });
+    });
+  });
+};
+
+// Routes that end in /plan
+// ----------------------------------------------------
+router.route('')
+// GET /api/plan
+  .get(getPlan)
 // POST /api/plan
-  .post(function(req, res) {
-  Plan.findOne({}, function(err, plan) {
-    if (err)
-      res.send(err);
-
-    if (plan) {
-      // Only save fields that have been passed, without editing ones that have not.
-      if (req.body.monthlyIncome !== undefined) { plan.monthlyIncome = req.body.monthlyIncome; }
-      if (req.body.monthlyBills !== undefined) { plan.monthlyBills = req.body.monthlyBills; }
-      if (req.body.monthlySavings !== undefined) { plan.monthlySavings = req.body.monthlySavings; }
-    } else {
-      plan = new Plan({
-        monthlyIncome: req.body.monthlyIncome,
-        monthlyBills: req.body.monthlyBills,
-        monthlySavings: req.body.monthlySavings
-      });
-    }
-
-    plan.save(function(err) {
-      if (err)
-        res.send(err);
-
-      res.json({ message: 'Plan Updated!' });
-    });
-  });
-})
+  .post(updatePlan)
 // PUT /api/plan
-  .put(function(req, res) {
-  Plan.findOne({}, function(err, plan) {
-    if (err)
-      res.send(err);
-
-    if (plan) {
-      // Only save fields that have been passed, without editing ones that have not.
-      if (req.body.monthlyIncome !== undefined) { plan.monthlyIncome = req.body.monthlyIncome; }
-      if (req.body.monthlyBills !== undefined) { plan.monthlyBills = req.body.monthlyBills; }
-      if (req.body.monthlySavings !== undefined) { plan.monthlySavings = req.body.monthlySavings; }
-    } else {
-      plan = new Plan({
-        monthlyIncome: req.body.monthlyIncome,
-        monthlyBills: req.body.monthlyBills,
-        monthlySavings: req.body.monthlySavings
-      });
-    }
-
-    plan.save(function(err) {
-      if (err)
-        res.send(err);
-
-      res.json({ message: 'Plan Updated!' });
-    });
-  });
-});
+  .put(updatePlan);
 
 module.exports = router;
