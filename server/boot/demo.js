3521a30a0fc90ed4d262a30bfc7a31eae2b20c26
Matthew Jacobs <m.a.jacobs@outlook.com>
2015-07-14 08:34:36 -0500

    Updated README. Updated demo script to create demo user only if one does not already exist.

--- server/boot/demo.js
+++ server/boot/demo.js
@@ -1,21 +1,26 @@
-module.exports = function(app) {
+module.exports = function (app) {
   var AccountHolder = app.models.AccountHolder;
 
-  AccountHolder.create([
-    {username: 'demo', email: 'demo@demo.com', password: 'demo'}
-  ], function(err, accountHolder) {
-    if (err) throw err;
+  AccountHolder.find({ where: { email: 'demo@demo.com' } }, function (err, ah) {
+    if (err || (ah.length > 0 && ah[0].id)) return;
+    
+    AccountHolder.create([
+      { username: 'demo', email: 'demo@demo.com', password: 'demo' }
+    ], function (err, accountHolder) {
+        if (err) throw err;
 
-    console.log('Created account holder:', accountHolder);
+        console.log('Created account holder:', accountHolder);
 
-    accountHolder[0].Plan.create({
-      monthlyIncome: 0,
-      monthlyBills: 0,
-      monthlySavings: 0
-    }, function(err, plan) {
-      if (err) throw err;
+        accountHolder[0].Plan.create({
+          monthlyIncome: 0,
+          monthlyBills: 0,
+          monthlySavings: 0
+        }, function (err, plan) {
+            if (err) throw err;
 
-      console.log('Created plan:', plan);
-    });
+            console.log('Created plan:', plan);
+          });
+      });
   });
+
 };
